9ab2ffb78a1025678db8538290785e2cb039344b
BrunaOliveira10 <bruna.oliveira10@estudante.ifto.edu.br>
2026-06-09 23:44:29 -0300

    first commit

---
+++ 2-Bimestre/JavaScript/aula01/js/script.js
@@ -0,0 +1,15 @@
+    document.getElementById("demo").innerHTML= "Pedrão";
+
+    function aumentar(){
+        document.getElementById("demo").style.fontSize = "35px";
+    }
+     function diminuir(){
+        document.getElementById("demo").style.fontSize = "10px";
+    }
+
+function Desligar(){
+        document.getElementById("Lampada").src = "img/lampada-apagada (1).png";
+    }
+     function Ligar(){
+    document.getElementById("Lampada").src = "img/lampada-iluminada (1).png";
+    }
---
+++ 2-Bimestre/JavaScript/aula09/script.js
@@ -0,0 +1,7 @@
+cument.getElementById("BtnMulher").addEventListener("click", function(){
+        document.getElementById("header").style.backgroundColor = "blueviolet";
+    }, false);
+
+    document.getElementById("BtnHomem").addEventListener("click", function(){
+        document.getElementById("header").style.backgroundColor = "blue";
+    }, false);
